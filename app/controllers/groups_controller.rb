70ef7f8dfc30135ac32a38c0cee4aa4610050019
melblstlr <melissaboelstler@chicagobooth.edu>
2020-04-19 06:36:35 +0000

    Hopefully this works, April 19

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,9 +2,8 @@ class GroupsController < ApplicationController
 
   def create
     @game =  Game.where({:code => params.fetch("query_game_code")}).at(0)
-    p "GAME ID ORIGINAL"
-    p @game.id 
     username = params.fetch("query_username")
+
     if username == nil
       redirect_to("/", { :notice => "Enter username" })
     end
@@ -13,23 +12,17 @@ class GroupsController < ApplicationController
     @user.username = username
     @user.password = "password"
     @user.email = @user.username + rand(1000).to_s + rand(1000).to_s + "@random.com"
+    @user.save
 
-    if @user.valid?
-      @user.save
-      session.store(:user_id, @user.id)
-   
-      @group = Group.new
-      @group.game_id = @game.id
-      @group.user_id = @user.id
-      @group.master = false
-      @group.save
+    #session.store(:user_id, @user.id)
+  
+    @group = Group.new
+    @group.game_id = @game.id
+    @group.user_id = @user.id
+    @group.master = false
+    @group.save
 
-
-      redirect_to("/games/#{@game.code}", { :notice => "Game joined successfully." })
-      #render({ :template => "games/show.html.erb" })
-    else
-      redirect_to("/", { :notice => "Failed to join game successfully." })
-    end
+    redirect_to("/games/#{@game.code}", { :notice => "Game joined successfully." })
   end
 
   def destroy
